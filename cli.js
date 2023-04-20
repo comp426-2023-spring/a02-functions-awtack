3c5059e474d69986a574f28ed5a79710c5f66402
Aaron Tackett <69739883+awtack@users.noreply.github.com>
2023-04-20 17:28:03 -0400

    GALOSH

--- cli.js
+++ cli.js
@@ -9,8 +9,65 @@ import moment from "moment-timezone";
 // process first args
 
 const args = minimist(process.argv.slice(2));
-const timezone = moment.tz.guess();
+const tizo = args.z || moment.tz.guess();
+const day = args.d || 1
+const lati = args.n || args.s * -1;
+const longi = args.e || args.w *-1;
 
-if(args.h) {
+
+const request = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude+ '&daily=precipitation_hours&timezone=' + timezone);
+const response = await request.json();
+
+
+// Getting help
+if('h' in args){
+    // If help
+    let help = `Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -t TIME_ZONE
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -t            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit.`
+    console.log(help);
+    process.exit(0);
+}
+if('j' in args){
+    console.log(response);
+    process.exit(0);
+}
+
+const daily_rain = response.daily.precipitation_hours[day]
+
+if(day==0){
+    if(daily_rain > 0){
+        console.log('Today is Rhianna time(get your Umbrella) you might need galoshes')
+    }
+    else{
+        console.log('Its probably pretty dry today, dont get out the galoshes')
+    }
+
+}
+else if(day==1){
+    if(daily_rain > 0){
+        console.log('Tomorrow is a good day for Gene Kelly(he will be singing in the rain) you might need galoshes')
+    }
+    else{
+        console.log('Its probably dry tomorrow, dont get out the galoshes')
+    }
+}
+else{
+    if(daily_rain>0){
+        console.log('You wont catch me outside without galoshes in'+day+'days')
+    }
+    else{
+        console.log('it will be a good day for sneakers(not galoshes) in'+day+'days')
+    }
+}
+
+
+
+
+
 
 
